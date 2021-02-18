5193e865b2a7ec2fb9d54dff8525300d6c2db491
wjacobc <wjacobchambers@gmail.com>
2021-02-18 03:49:32 -0500

    Added New York Times Health RSS feed function

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -66,11 +66,42 @@ def nih_rss():
     return newsitems
 
 
+def nyt_rss():
+    nyt = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/Health.xml")
+    entries = nyt["entries"]
+
+    newsitems = []
+    for entry in entries:
+        # convert the string date representation into a python datetime object
+        date_object = datetime.strptime(entry["published"], "%a, %d %b %Y %H:%M:%S %z")
+        # remove the time zone offset so we can compare it to other articles
+        date_object = date_object.replace(tzinfo = None)
+
+        headline = entry["title"]
+        source = "The New York Times"
+        publish_date = date_object
+        # some NYT articles have empty snippets, should we skip these 
+        # or try to create one?
+        snippet = entry["summary"]
+        url = entry["link"]
+        tags = match_tags(headline, snippet)
+        # NYT has tags in entry["tags"] - do we want to use these?
+        # problem is they will likely not line up with the ones we generate
+
+        newsitem_entry = NewsItem(headline, source, publish_date, snippet, url, tags)
+        newsitems.append(newsitem_entry)
+
+    return newsitems
+
+
 def collect_articles():
     nih_articles = nih_rss()
     cdc_articles = cdc_rss()
-    articles = nih_articles + cdc_articles
+    nyt_articles = nyt_rss()
+    articles = nih_articles + cdc_articles + nyt_articles
 
+    # just get the articles that have been added to the RSS feeds since the
+    # last time we ran the scraper
     last_ran_scraper = firebase_last_ran()
     recent_articles = [article for article in articles if article.publish_date > last_ran_scraper]
 
