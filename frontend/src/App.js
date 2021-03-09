5ecc52a4cc2455ac8a7e1963d7473b4b4bd21845
sam1993316 <54906199+sam1993316@users.noreply.github.com>
2021-03-08 22:01:31 -0500

    added connection to firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./firebase_config";
+import { useEffect } from "react";
+
+let newsitems = [];
 
 function App() {
+  // the hook that retrieves all newsitems from database
+  useEffect(() => {
+    let ref = firebase.database().ref("newsitems");
+    ref.on("value", (snapshot) => {
+      newsitems = snapshot.val();
+      // test
+      console.log(newsitems);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
