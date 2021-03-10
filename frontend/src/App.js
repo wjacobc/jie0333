a1164676e627010cbd493997c9e9747bed77f41b
Jacob Chambers <wchambers8@gatech.edu>
2021-03-10 14:29:42 -0500

    Basic chakra setup, move Firebase connection to independent function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import firebase from "./firebase_config";
-import { useEffect } from "react";
+import { ChakraProvider, Heading } from "@chakra-ui/react";
 
 let newsitems = [];
 
-function App() {
-  // the hook that retrieves all newsitems from database
-  useEffect(() => {
+function getFirebaseNewsItems() {
     let ref = firebase.database().ref("newsitems");
     ref.on("value", (snapshot) => {
-      newsitems = snapshot.val();
-      // test
-      console.log(newsitems);
+        newsitems = snapshot.val();
+        // test
+        return newsitems;
     });
-  });
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {
+    return (
+        <ChakraProvider>
+            <Heading>Testing Chakra elements</Heading>
+        </ChakraProvider>
+    );
 }
 
 export default App;
