8fb81085cdf2cabed0a8b389ff4f7bed8dc44008
Jacob Chambers <wchambers8@gatech.edu>
2021-04-23 23:56:14 -0400

    Update frontend data processing functionality to match new data storage format for tags in Firebase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Newsfeed from './Newsfeed';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
   	<ChakraProvider>
-    	<App />
+    	<Newsfeed />
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
